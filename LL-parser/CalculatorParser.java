7226bb2a7a5d7562610efd0b911f95889bdce65d
VasiaKoum <vasiakoum@hotmail.com>
2020-03-23 23:02:31 +0200

    change functions, add lookahead table & grammar

--- LL-parser/CalculatorParser.java
+++ LL-parser/CalculatorParser.java
@@ -2,7 +2,7 @@ import java.io.InputStream;
 import java.io.IOException;
 
 class CalculatorParser{
-    private int lookaheadToken, check_digit;
+    private int lookaheadToken, check_digit, x;
     private InputStream in;
 
     public CalculatorParser(InputStream in) throws IOException{
@@ -16,44 +16,76 @@ class CalculatorParser{
     }
 
     private void Exp1() throws IOException, ParseError{
-        System.out.printf("Exp1: lookaheadToken: [%c]\n", lookaheadToken);
-        if (lookaheadToken == '\n' || lookaheadToken == -1 || lookaheadToken == '\r')
-            throw new ParseError();
-        Term1(); Exp2();
+        // System.out.printf("Exp1: lookaheadToken: [%c]\n", lookaheadToken);
+        if((lookaheadToken >= '0' && lookaheadToken <= '9') || lookaheadToken == '('){
+            Term1(); Exp2();
+        }
+        else throw new ParseError();
     }
 
     private void Exp2() throws IOException, ParseError{
-        if(lookaheadToken == '\n' || lookaheadToken == -1 || lookaheadToken != '\r')
+        // System.out.printf("Exp2: lookaheadToken: [%c]\n", lookaheadToken);
+        if(lookaheadToken == '\n' || lookaheadToken == -1 || lookaheadToken == '\r' || lookaheadToken == ')')
     	    return;
-    	if(lookaheadToken == '+'){
-            consume('+');
+        else if(lookaheadToken == '+' || lookaheadToken == '-'){
+            consume(lookaheadToken);
             Term1(); Exp2();
-            return;
         }
-    	if(lookaheadToken == '-'){
-            consume('-');
-            Term1(); Exp2();
-            return;
+        else throw new ParseError();
+    }
+
+    private void Term1() throws IOException, ParseError{
+        // System.out.printf("Term1: lookaheadToken: [%c]\n", lookaheadToken);
+        if((lookaheadToken >= '0' && lookaheadToken <= '9') || lookaheadToken == '('){
+            Factor(); Term2();
         }
+        else throw new ParseError();
 
     }
 
-    private void Num() throws IOException, ParseError{
-        check_digit = in.read();
-        if(check_digit >= '0' && check_digit <= '9'){
-            lookaheadToken = check_digit;
-            Digit(); Num();
+    private void Term2() throws IOException, ParseError{
+        // System.out.printf("Term2: lookaheadToken: [%c]\n", lookaheadToken);
+        x = lookaheadToken;
+        if((lookaheadToken >= '0' && lookaheadToken <= '9') || lookaheadToken == '(') throw new ParseError();
+        else if(lookaheadToken == '*' || lookaheadToken == '/'){
+            consume(lookaheadToken);
+            Factor(); Term2();
         }
-        else{
-            if(lookaheadToken >= '0' && lookaheadToken <= '9') Digit();
-            else throw new ParseError();
+        else if(x=='+'||x=='-'||x==')'||x=='\n'||x==-1||x=='\r')
+            return;
+    }
+
+    private void Factor() throws IOException, ParseError{
+        // System.out.printf("Factor: lookaheadToken: [%c]\n", lookaheadToken);
+        if(lookaheadToken >= '0' && lookaheadToken <= '9') Num1();
+        else if(lookaheadToken == '(') {
+            consume(lookaheadToken);
+            Exp1();
+            consume(')');
         }
+        else throw new ParseError();
+    }
+
+    private void Num1() throws IOException, ParseError{
+        // System.out.printf("Num1: lookaheadToken: [%c]\n", lookaheadToken);
+        if(lookaheadToken < '0' || lookaheadToken > '9') throw new ParseError();
+        else { Digit(); Num2(); }
+    }
+
+    private void Num2() throws IOException, ParseError{
+        // System.out.printf("Num2: lookaheadToken: [%c]\n", lookaheadToken);
+        x = lookaheadToken;
+        if(lookaheadToken >= '0' && lookaheadToken <= '9') { Digit(); Num2();}
+        if(x=='+'||x=='-'||x=='/'||x=='*'||x==')'||x=='\n'||x==-1||x=='\r')
+            return;
+        else throw new ParseError();
+
     }
 
     private void Digit() throws IOException, ParseError{
-        if(lookaheadToken < '0' || lookaheadToken > '9')
-            throw new ParseError();
-        consume(lookaheadToken);
+        // System.out.printf("Digit: lookaheadToken: [%c]\n", lookaheadToken);
+        if(lookaheadToken < '0' || lookaheadToken > '9') throw new ParseError();
+        else consume(lookaheadToken);
     }
 
     public void parse() throws IOException, ParseError{
